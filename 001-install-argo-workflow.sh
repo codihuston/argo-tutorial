03719590df97848fac90e7efc94fcb7e90f85dda
Codi Huston <codi.huston@cyberark.com>
2023-02-04 00:08:28 -0600

    Add steps to install argo workflow

---
+++ 001-install-argo-workflow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+main(){
+  ARGO_WORKFLOWS_VERSION="3.4.4"
+  kubectl create namespace argo
+  kubectl apply -n argo -f "https://github.com/argoproj/argo-workflows/releases/download/v$ARGO_WORKFLOWS_VERSION/install.yaml"
+
+  kubectl patch deployment \
+    argo-server \
+    --namespace argo \
+    --type='json' \
+    -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": [
+    "server",
+    "--auth-mode=server"
+  ]}]'
+
+  argo submit -n argo --watch https://raw.githubusercontent.com/argoproj/argo-workflows/master/examples/hello-world.yaml
+
+  argo list -n argo
+
+  argo get -n argo @latest
+}
+
+main "$@"
--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Purpose
 
 To demo Argo Workflow, Events, and CD.
+
+### Prerequisites
+
+1. [Install Argo CLI](https://github.com/argoproj/argo-workflows/releases)
+2. [Kubernetes Cluster](https://kind.sigs.k8s.io/)
+
+
+## Argo Workflow (CI)
+
+Install and test a sample workflow:
+
+```
+./install-argo-workflow.sh
+```
+
+Port forward the UI:
+
+```
+kubectl -n argo port-forward deployment/argo-server 2746:2746
+```
+
+### Getting Started
+
+## Argo CD
+
+## Argo Rollouts
+
+## Argo Events
+
+To kick off a workflow, CD, or Rollout, we use Argo Events.
